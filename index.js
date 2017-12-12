7fe1e886420d9787a40e8c3e5abb39c3cecd91fd
Kuwar Nagpal <kuwar.nagpal@gmail.com>
2017-12-12 01:28:16 -0500

    Added Soundcloud link support.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-let linksOrSearchQueries = //Put queries or links between the backticks, and run the file, I need to learn how to make GUIs.
+let linksOrSearchQueries = //Put queries or links between the backticks, and run the file. Also supports soundcloud links, but not searches.
 `
-Rip ROach XXXtentaction
-Yung bRatz xxtentacion
+https://www.youtube.com/watch?v=BnANUza_uXQ
 `
-//I'm dead this is so useful
 
+const sc = require ('soundcloud-dl');
+const request = require('request-promise')
 const YouTube = require('youtube-node');
 const youtube = new YouTube();
 const fs      = require('fs');
-const secure  = require('./secure.json'); // Replace secure.key, and this line
-youtube.setKey(secure.key);               // With your youtube API key.
+const secure  = require('./secure.json'); // Be sure to add your YouTube API Key to ./secure.json.
+youtube.setKey(secure.youtubeAPIKey);
 var YoutubeMp3Downloader = require("youtube-mp3-downloader");
+
 var YD = new YoutubeMp3Downloader({
    "ffmpegPath": "./ffmpeg.exe", //The single ffmpeg binary location.
    "outputPath": "./Downloads",
@@ -18,23 +19,38 @@ var YD = new YoutubeMp3Downloader({
    "queueParallelism": 3,
    "progressTimeout": 2000
 });
-
 let queries = linksOrSearchQueries.split("\n");
-let cleanQueries = []
-for(let i = 0;i<queries.length;i++) {
-    if(queries[i] != "") cleanQueries.push(queries[i]);
+let youtubeCleanQueries = [];
+let soundcloudQueries = [];
+for(let i = 0; i<queries.length; i++) {
+    if(queries[i] != "") {
+        queries[i].toLowerCase().indexOf('soundcloud') > -1 ? soundcloudQueries.push(queries[i]) : youtubeCleanQueries.push(queries[i]);
+    }
 }
-if(cleanQueries.length < 1) return console.log("There are no videos to download.");
+
+if(youtubeCleanQueries.length < 1 && soundcloudQueries.length < 1) return console.log("There are no videos to download.");
+
 YD.on("finished", function(err, data) {
     console.log(`Finished downloading: ${data.videoTitle}`);
 });
-cleanQueries.forEach(query => {
+
+youtubeCleanQueries.forEach(query => {
     youtube.search(query, 1, {type: "video"}, async (e, r) => {
         if(!r.items[0]) return console.log(`There was no video for the search: ${query}, skipping it.`);
         songObject = {title: r.items[0].snippet.title, url: r.items[0].id.videoId};
         return new Promise((resolve, reject) => {
             YD.download(songObject.url);
             resolve(songObject.title);
-        }).then(title => console.log("Download queued: " + title));
+        }).then(title => console.log(`Download queued: ${title}`));
     });
 });
+
+soundcloudQueries.forEach(query => {
+    sc.getSongDlByURL(query).then((song) => {
+        request(song.http_mp3_128_url).pipe(
+            fs.createWriteStream(`./Downloads/${query.replace(/soundcloud|.com|https|[/\\?:=]/gi, '').replace('-', ' ')}.mp3`)
+            .on('finish', () => {console.log(`Finished downloading: ${query}`)})
+        );
+    });
+    console.log(`Download queued: ${query.substring(0, 30)}...`);
+})
